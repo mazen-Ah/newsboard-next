4591a52a14d5bf334287670c408b7e6d61227369
mazen-A <58402268+mazen-Ah@users.noreply.github.com>
2025-08-04 16:53:36 +0300

    add global variables env

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -7,13 +7,13 @@ export const apiClient = axios.create({
 export const newsApiClient = axios.create({
   baseURL: "https://newsapi.org/v2",
   headers: {
-    "X-API-Key": process.env.NEXT_PUBLIC_NEWSAPI_KEY,
+    "X-API-Key": process.env.NEXT_PUBLIC_NEWSAPI_KEY  || "407126cc0152479cb6a3c875f402f789",
   },
 });
 
 export const guardianApiClient = axios.create({
   baseURL: "https://content.guardianapis.com",
   params: {
-    "api-key": process.env.NEXT_PUBLIC_GUARDIAN_API_KEY,
+    "api-key": process.env.NEXT_PUBLIC_GUARDIAN_API_KEY || "96dbf704-c5dd-4581-84c4-ca9f049244d1",
   },
 });
